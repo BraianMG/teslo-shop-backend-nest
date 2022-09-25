857bf92933e58fdcee721574a271609e32f5081a
Braian <braian.gonzales77@gmail.com>
2022-09-25 17:29:27 -0300

    crear relación entre productos e imágenes

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,10 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ProductImage } from './';
 
 @Entity()
 export class Product {
@@ -52,8 +54,11 @@ export class Product {
     default: [],
   })
   tags: string[];
-  
-  // images
+
+  @OneToMany(() => ProductImage, (productImage) => productImage.product, {
+    cascade: true,
+  })
+  images?: ProductImage;
 
   @BeforeInsert()
   checkSlugInsert() {
