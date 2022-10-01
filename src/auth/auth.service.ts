242ef798b5b42d99df938ac381b141035d40b76e
Braian <braian.gonzales77@gmail.com>
2022-10-01 20:51:15 -0300

    agregar id de usuario en payload de JWT token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -33,7 +33,7 @@ export class AuthService {
       await this.userRepository.save(user);
       delete user.password;
 
-      return { ...user, token: this.getJwtToken({ email: user.email }) };
+      return { ...user, token: this.getJwtToken({ id: user.id }) };
     } catch (error) {
       this.handleDBExceptions(error);
     }
@@ -44,7 +44,7 @@ export class AuthService {
 
     const user = await this.userRepository.findOne({
       where: { email },
-      select: { email: true, password: true },
+      select: { email: true, password: true, id: true },
     });
 
     if (!user)
@@ -53,7 +53,7 @@ export class AuthService {
     if (!bcrypt.compareSync(password, user.password))
       throw new UnauthorizedException('Credentials are not valid (password)');
 
-    return { ...user, token: this.getJwtToken({ email: user.email }) };
+    return { ...user, token: this.getJwtToken({ id: user.id }) };
   }
 
   private getJwtToken(payload: JwtPayload) {
--- src/auth/interfaces/jwt-payload.interface.ts
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -1,4 +1,4 @@
 export interface JwtPayload {
-  email: string;
+  id: string;
   // TODO: Agregar todo lo que se quiera grabar (nada sensible)
 }
