07346c798517667aca0940fa0561c466a366444b
Braian <braian.gonzales77@gmail.com>
2022-10-01 20:15:53 -0300

    generar JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,6 +10,8 @@ import { Repository } from 'typeorm';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
@@ -18,6 +20,7 @@ export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
@@ -30,8 +33,7 @@ export class AuthService {
       await this.userRepository.save(user);
       delete user.password;
 
-      return user;
-      // TODO: Retornar JWT de acceso
+      return { ...user, token: this.getJwtToken({ email: user.email }) };
     } catch (error) {
       this.handleDBExceptions(error);
     }
@@ -51,8 +53,12 @@ export class AuthService {
     if (!bcrypt.compareSync(password, user.password))
       throw new UnauthorizedException('Credentials are not valid (password)');
 
-    return user;
-    // TODO: Retornar JWT de acceso
+    return { ...user, token: this.getJwtToken({ email: user.email }) };
+  }
+
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 
   private handleDBExceptions(error: any): never {
