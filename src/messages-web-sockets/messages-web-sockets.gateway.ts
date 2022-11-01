f598a229343bdca0a41bcb5b09ccc3c9f55b3476
Braian <braian.gonzales77@gmail.com>
2022-11-01 18:12:00 -0300

    emitir mensajes a los clientes

--- src/messages-web-sockets/messages-web-sockets.gateway.ts
+++ src/messages-web-sockets/messages-web-sockets.gateway.ts
@@ -2,16 +2,20 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketGateway,
-  WebSocketServer
 } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketServer,
+} from '@nestjs/websockets/decorators';
 import { Socket, Server } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
 import { MessagesWebSocketsService } from './messages-web-sockets.service';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWebSocketsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
-  @WebSocketServer() wss: Server
+  @WebSocketServer() wss: Server;
   constructor(
     private readonly messagesWebSocketsService: MessagesWebSocketsService,
   ) {}
@@ -19,12 +23,24 @@ export class MessagesWebSocketsGateway
   handleConnection(client: Socket, ...args: any[]) {
     this.messagesWebSocketsService.registerClient(client);
 
-    this.wss.emit('clients-updated', this.messagesWebSocketsService.getConnectedClients())
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWebSocketsService.getConnectedClients(),
+    );
   }
 
   handleDisconnect(client: Socket) {
     this.messagesWebSocketsService.removeClient(client.id);
 
-    this.wss.emit('clients-updated', this.messagesWebSocketsService.getConnectedClients())
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWebSocketsService.getConnectedClients(),
+    );
+  }
+
+  // message-from-client
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    console.log({ clientId: client.id, payload });
   }
 }
